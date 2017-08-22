c1bb1a561bd0d1002fa0be437da1a5ce572eed7e
wys702a6 <wys702a6@aliyun.com>
2017-08-22 11:35:20 +0800

    Update configdb.py

--- InterfaceAuto/src/interface/configdb.py
+++ InterfaceAuto/src/interface/configdb.py
@@ -13,17 +13,19 @@ import sys
 class ConfigDB(object):
     '''配置数据库ip，端口等信息，获取数据库连接'''
     def __init__(self, ini_file, db):
+        self.ini_file = ini_file
+        self.db = db
+        
         config = ConfigParser()
-        
         #从配置文件中读取数据库服务器ip,域名，端口
-        config.read(ini_file)
+        config.read(self.ini_file)
         
-        self.host = config.get('DATABASE1','host')
-        self.port = config.get('DATABASE1','port')
-        self.user = config.get('DATABASE1','user')
-        self.passwd = config.get('DATABASE1','password')
-        self.db = config.get('DATABASE1','db')
-        self.charset = config.get('DATABASE1','charset')
+        self.host = config.get(self.db,'host')
+        self.port = config.get(self.db,'port')
+        self.user = config.get(self.db,'user')
+        self.passwd = config.get(self.db,'password')
+        self.db = config.get(self.db,'db')
+        self.charset = config.get(self.db,'charset')
 
     def get_conn(self):
         try:
